e22e905968bfa70bc69046cd63c5b20b9366697b
Mahrs <mahrsbar22@gmail.com>
2023-02-05 21:46:59 -0700

    Fix name

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
-import Weather from "../components/weather"
-
+import Weather from "../components/Weather"
 
 const IndexPage = () => (
   <Layout>
